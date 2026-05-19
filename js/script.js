7cabcc67ecb9b351775a1a0b7b0bef97a36ceaff
evelynr1 <evelynr@umich.edu>
2026-05-19 16:30:19 -0400

    moved parks list to homepage. carousel works but next/prev buttons do not yet

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// set up functions
+function next(){
+
+}
+
+function previous(){
+
+}
+
+
+// add eventListeners
+const next = document.querySelector("carousel-control-next");
+const prev = document.querySelector("carousel-control-prev");
+
+next.addEventListener("click", function () {
+    
+});
+
+helloBtn.addEventListener("click", function () {
+  message.innerHTML = "Hello!";
+});
+
+
+//putting it all together
+
