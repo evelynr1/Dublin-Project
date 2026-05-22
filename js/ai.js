f6e6fc492e409ffc9ceb15c674b9c64789be8d4a
evelynr1 <evelynr@umich.edu>
2026-05-22 06:00:04 -0400

    working on mobile nav

---
+++ js/ai.js
@@ -0,0 +1,12 @@
+// code for mobile navbar from https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+
+
+// /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
--- js/script.js
+++ js/script.js
@@ -1,24 +1,65 @@
-// set up functions
-function next(){
+// // set up functions
+// function next(){
 
-}
+// }
 
-function previous(){
+// function previous(){
 
-}
+// }
 
 
-// add eventListeners
-const next = document.querySelector("carousel-control-next");
-const prev = document.querySelector("carousel-control-prev");
+// // add eventListeners
+// const next = document.querySelector("carousel-control-next");
+// const prev = document.querySelector("carousel-control-prev");
 
-next.addEventListener("click", function () {
+// next.addEventListener("click", function () {
     
+// });
+
+// helloBtn.addEventListener("click", function () {
+//   message.innerHTML = "Hello!";
+// });
+
+
+//mobile nav hamburger menu
+// document.querySelector("#hamburger").addEventListener("click", function () {
+//   this.style.display = "none";
+//   });
+
+const mobileNav = document.querySelector("#hamburger");
+// it works to turn the correct area yellow, but not to set it to display:none
+// mobileNav.addEventListener("click", function () {
+//   console.log("hamburger menu");
+//   // document.querySelector(".topnav #myLinks").classList.toggle("open");
+//   document.querySelector(".topnav #myLinks").style.backgroundColor = "yellow";
+  
+//   console.log(document.querySelector(".topnav #myLinks").className);
+// })
+
+//  adding a border during hover or focus within navbar
+navLinks = document.querySelectorAll(".topnav a");
+navLinks.forEach(function(navLink){
+  navLink.addEventListener("mouseover", function () {
+    navLink.classList.add("activeNavLink");
+    console.log("nav link border 1");
+    console.log(navLink);
+  });
+  navLink.addEventListener("focus", function () {
+    console.log("nav link border 2");
+    navLink.classList.add("activeNavLink");
+  });
+  navLink.addEventListener("mouseout", function () {
+    console.log("nav link border 3");
+    navLink.classList.remove("activeNavLink");
+  });
+  navLink.addEventListener("blur", function () {
+    console.log("nav link border 4");
+    navLink.classList.remove("activeNavLink");
+  });
 });
 
-helloBtn.addEventListener("click", function () {
-  message.innerHTML = "Hello!";
-});
+// background-color: var(--accent3);
+//   color: black;
 
 
 //putting it all together
