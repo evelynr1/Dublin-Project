7ca16ea3e6a7ca376855acb5a1cf76e33bf9ddc1
evelynr1 <evelynr@umich.edu>
2026-05-24 15:12:46 -0400

    rearranged the pages I will be including and redid the nav bar to accommodate that

--- js/ai.js
+++ js/ai.js
@@ -1,6 +1,4 @@
 // code for mobile navbar from https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
-
-
 // /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function mobileNavMenu() {
   var x = document.getElementById("myLinks");
@@ -10,4 +8,25 @@ function mobileNavMenu() {
     x.style.display = "block";
     x.style.height = "90vh";
   }
+}
+
+// code for scrool to top button from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
